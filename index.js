68f0fb4bb87b032842d500312a023bada0944d5c
YuhoHarada <yuhoharada0616aaa@gmail.com>
2020-10-08 08:56:27 +0200

    page

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cookieSession = require('cookie-session')
 require('./config/passport-setup')
 const passport = require('passport');
 const keys = require('./config/keys')
-const PORT = process.env.PORT||3000
+const PORT = process.env.PORT || 3000
+const User = require('./models/user')
 
 mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -24,7 +25,45 @@ app.use(passport.session())
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index', { user: req.user })
+    if (req.user == undefined) {
+        res.render('index', { user: req.user })
+    } else {
+        console.log(req.user);
+        res.redirect('/user/menu/' + req.user._id)
+    }
+})
+
+app.get('/user/menu/:id', (req, res) => {
+    res.render('menu', { user: req.user })
+})
+
+app.get('/user/mypage/:id', (req, res) => {
+    res.render('mypage', { user: req.user })
+})
+
+app.get('/course/:id/:coursename', (req, res) => {
+    res.render('course', { user: req.user, coursename: req.params.coursename })
+})
+
+app.get('/start/:id/:coursename/:chapter', async (req, res) => {
+    let doc = await User.findOne({ _id: req.params.id })
+    let update = []
+    if (doc.skillsProgress != undefined) {
+        update = doc.skillsProgress
+    }
+    update.push({
+        chapter: req.params.chapter,
+        start: new Date()
+    })
+    console.log(update);
+    let update1 = { $set: { skillsProgress: update } }
+    console.log(update1);
+    User.findOneAndUpdate({ _id: req.params.id }, update1, { new: true, useFindAndModify: true })
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => console.error(`Failed to find and update document: ${err}`))
+    res.redirect('https://www.udemy.com/de/')
 })
 
 app.get('/auth/google',
